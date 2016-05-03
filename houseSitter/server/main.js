65a2e515ebe7f45d0d2a920f9ab7af540115a1d8
Michael Luo <mluo@Michaels-MacBook-Pro.local>
2016-05-03 15:10:49 +1000

    step one

---
+++ houseSitter/server/main.js
@@ -0,0 +1,25 @@
+import {
+    Meteor
+} from 'meteor/meteor';
+
+Meteor.startup(() => {
+    // code to run on server at startup
+    if (HousesCollection.find().count() === 0) {
+        var houses = [{
+            name: 'Stephan',
+            plants: [{
+                color: 'red',
+                instructions: '3 pots/week'
+            }, {
+                color: 'white',
+                instructions: 'keep humid'
+            }]
+        }];
+
+        while (houses.length > 0) {
+            HousesCollection.insert(houses.pop());
+        }
+        console.log('Added fixtures');
+    }
+
+});
