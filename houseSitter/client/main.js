34dcc0ca7c5b49785ff7cb3a5195eb5be6085609
Michael Luo <mluo@Michaels-MacBook-Pro.local>
2016-05-03 16:49:59 +1000

    add timestamp into databse

--- houseSitter/client/main.js
+++ houseSitter/client/main.js
@@ -1,22 +1,52 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {
+    Template
+} from 'meteor/templating';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+Tracker.autorun(function() {
+    console.log("The selectedHouse ID is: " +
+        Session.get("selectedHouseId")
+    );
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.selectHouse.helpers({
+    housesNameId: function() {
+        return HousesCollection.find({});
+    },
+    isSelected: function() {
+        return Session.equals('selectedHouseId', this._id) ? 'selected' : '';
+    }
+});
+Template.selectHouse.events = {
+    'change #selectHouse': function(evt) {
+        Session.set('selectedHouseId', evt.currentTarget.value);
+    }
+};
+
+
+Template.showHouse.helpers({
+    house: function() {
+        return HousesCollection.findOne({
+            _id: Session.get("selectedHouseId")
+        });
+    }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+
+
+Template.plantDetails.events({
+    'click button.water': function(evt) {
+        var plantId = $(evt.currentTarget).attr('data-id');
+        Session.set(plantId, true);
+        var lastvisit = new Date();
+        HousesCollection.update({
+            _id: Session.get('selectedHouseId')
+        }, {
+            $set: {
+                lastvisit: lastvisit
+            }
+        });
+    }
 });
--- houseSitter/server/main.js
+++ houseSitter/server/main.js
@@ -4,22 +4,35 @@ import {
 
 Meteor.startup(() => {
     // code to run on server at startup
-    if (HousesCollection.find().count() === 0) {
-        var houses = [{
-            name: 'Stephan',
-            plants: [{
-                color: 'red',
-                instructions: '3 pots/week'
-            }, {
-                color: 'white',
-                instructions: 'keep humid'
-            }]
+    var houses = [{
+          name: 'Manuel',
+          plants: [{
+            color: 'Red',
+            instructions: '3 pots/week'
+          }, {
+            color: 'Yellow',
+            instructions: 'keep humid'
+          }]
+        }, {
+          name: 'Stephan',
+          plants: [{
+            color: 'Red',
+            instructions: '3 pots/week'
+          }, {
+            color: 'White',
+            instructions: 'water daily'
+          }],
+          animals: [ // this will never be used in our application
+            {
+              name: 'Danbo',
+              instructions: '1 carrot/day'
+            }
+          ]
         }];
 
         while (houses.length > 0) {
-            HousesCollection.insert(houses.pop());
+          HousesCollection.insert(houses.pop());
         }
         console.log('Added fixtures');
-    }
-
+      
 });
