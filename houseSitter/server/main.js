8afd65979091fb36a542290638795a5e4e6b4b82
Michael Luo <mluo@Michaels-MacBook-Pro.local>
2016-05-05 09:43:40 +1000

    constrain start insertion

--- houseSitter/server/main.js
+++ houseSitter/server/main.js
@@ -4,35 +4,36 @@ import {
 
 Meteor.startup(() => {
     // code to run on server at startup
-    var houses = [{
-          name: 'Manuel',
-          plants: [{
-            color: 'Red',
-            instructions: '3 pots/week'
-          }, {
-            color: 'Yellow',
-            instructions: 'keep humid'
-          }]
+    if (HousesCollection.find().count() === 0) {
+        var houses = [{
+            name: 'Manuel',
+            plants: [{
+                color: 'Red',
+                instructions: '3 pots/week'
+            }, {
+                color: 'Yellow',
+                instructions: 'keep humid'
+            }]
         }, {
-          name: 'Stephan',
-          plants: [{
-            color: 'Red',
-            instructions: '3 pots/week'
-          }, {
-            color: 'White',
-            instructions: 'water daily'
-          }],
-          animals: [ // this will never be used in our application
-            {
-              name: 'Danbo',
-              instructions: '1 carrot/day'
-            }
-          ]
+            name: 'Stephan',
+            plants: [{
+                color: 'Red',
+                instructions: '3 pots/week'
+            }, {
+                color: 'White',
+                instructions: 'water daily'
+            }],
+            animals: [ // this will never be used in our application
+                {
+                    name: 'Danbo',
+                    instructions: '1 carrot/day'
+                }
+            ]
         }];
 
         while (houses.length > 0) {
-          HousesCollection.insert(houses.pop());
+            HousesCollection.insert(houses.pop());
         }
         console.log('Added fixtures');
-      
+    }
 });
